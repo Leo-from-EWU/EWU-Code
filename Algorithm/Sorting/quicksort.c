5b47a9ac8d07d816f3fbdb320f0cd2e4e846134e
Samiul Azim <samiulazim@Leos-MacBook-Air.local>
2026-07-04 00:33:50 +0600

    input structures has introduced

--- Algorithm/Sorting/quicksort.c
+++ Algorithm/Sorting/quicksort.c
@@ -1,52 +1,72 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int partion(int arr[],int lb,int up);
-
-void quicksort(int arr[],int lb,int up){
-
-    if(lb<up){
-        int p= partion(arr,lb,up);
-        quicksort(arr,lb,p-1);
-        quicksort(arr,p+1,up);
-    }
-}
-
-int partion(int arr[],int lb,int up){
-    int p=lb;
-    int lower= lb+1;
-    int upper= up;
+int partition(int arr[], int lb, int ub)
+{
+    int pivot = lb;
+    int start = lb + 1;
+    int end = ub;
     int temp;
-    while(lower <= upper){
-    while(lower <= up && arr[lower] <= arr[p])
-    lower++;
 
-    while(upper >= lb && arr[upper] > arr[p])
-    upper--;
+    while (start <= end)
+    {
+        while (arr[start] <= arr[pivot])
+        {
+            start++;
+        }
+        while (arr[end] > arr[pivot])
+        {
+            end--;
+        }
 
-    if(lower<upper){
-         temp= arr[lower];
-        arr[lower]= arr[upper];
-        arr[upper]=temp;
+        if (start < end)
+        {
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+        }
+    }
+    temp = arr[pivot];
+    arr[pivot] = arr[end];
+    arr[end] = temp;
+    return end;
+}
+
+
+void quicksort(int arr[], int lb, int ub)
+{
+    if (lb < ub) 
+    { 
+        int loc = partition(arr, lb, ub); // giving endinfg
+        quicksort(arr, lb, loc - 1);
+        quicksort(arr, loc + 1, ub);
     }
 }
-    temp= arr[p];
-    arr[p]= arr[upper];
-    arr[upper]=temp;
-    return upper;
+int main()
+{
 
-}
+    int size; // for array input size
 
+    printf("Enter the size of the Array: \n");
+    scanf("%d", &size);
 
-int main(){
-    int arr[]={7,6,10,5,9,2,1,15,7};
+    int arr[size];
+    printf("Enter the elements of the array:\n");
 
-    quicksort(arr,0,8);
-
-    for(int i=0;i<9;i++){
-        printf("%d\n",arr[i]);
+    // input
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
     }
+    quicksort(arr, 0, size - 1); // sending low=0, and up
 
+    //------ print------
+
+    printf("Quick Sort Array Elements :\n");
+    for (int i = 0; i < size; i++)
+    {
+
+        printf("%d Element -%d\n", i + 1, arr[i]);
+    }
 
     return 0;
 }
