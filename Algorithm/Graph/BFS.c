017d70f9ce074626a074353600bf8514590fb776
Samiul Azim <samiulazim@Leos-MacBook-Air.local>
2026-08-08 00:56:27 +0600

    Improve BFS input for directed and weighted graphs

--- Algorithm/Graph/BFS.c
+++ Algorithm/Graph/BFS.c
@@ -1,93 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define m 100
 
 
-#define MAX 100
-#define WHITE 0 //white = unvisted
-#define GRAY 1  //visited
-#define BLACK 2 // visited & completed
+// This BFS introduced for directed and wighted graph also
+
+# define w 0 //White = unvisted
+#define g 1 // Gary visited
+#define b 2// Black visited & completed
+
 int n;
-int adj[MAX][MAX];
-
-int color[MAX];
-int distance[MAX];
-int prev[MAX];  // Parent checking. 
-// like 0,1,3 is connected and
-//  we chose 2 no parent then went to 3 ,1 is parent.
-
-
-
+int adj[m][m];
+int color[m];
+int parent[m];
+int distance[m];
 
 void BFS(int s){
-    int queue[MAX];
-    int front=0,rear=0;
-    
+    int que[m]; // queue
+    int front=0, rear=0;
+
     for(int i=0;i<n;i++){
-    if(i!=s){
-        color[i]=WHITE;
-        distance[i]=-1;
-        prev[i]=-1;
-
-    }}
-
-    color[s]=GRAY;
-    distance[s]=0;
-    prev[s]=-1;
-    queue[rear++]=s;
-
-    while(front<rear){
-        int u= queue[front++]; //1
-        for(int v=0;v<n;v++){
-            if(adj[u][v]==1 && color[v]==WHITE){
-                color[v]=GRAY;
-                distance[v]=distance[u]+1;
-                prev[v]=u;
-                queue[rear++]=v;
-            }
+        if(s!=i){
+            color[i]=w;
+            distance[i]=-1;
+            parent[i]=-1;
         }
-        color[u]=BLACK;
+        else{
+            color[s]=g;
+            distance[s]=0;
+            parent[s]=-1;
+            que[rear]=s;
+            rear=rear+1;
+        }
+        }
+
+        while (front<rear)
+        {
+            int u= que[front];
+            front= front+1;
+            for(int v=0;v<n;v++){
+                if(adj[u][v]!=0 && color[v]==w){
+                    color[v]=g;
+                    distance[v]=distance[u]+1;
+                    parent[v]=u;
+                    que[rear]=v;
+                    rear++;
+                }
+            }
+            color[u]=b;
+        }
+        
+
+
+
 
     }
 
 
     
-}
+
+
 
 int main(){
     int e;
-    printf("Enter the vertices of the array: \n");
+    printf("Enter the num of vertices: ");
     scanf("%d",&n);
     
 
-    printf("Enter the egdes of the array: \n");
-    scanf("%d",&e);
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            adj[i][j]=0;
-
+    printf("Enter the egdes: (u,v)\n ");
+    
+    
+    for(int u=0;u<n;u++){
+        for(int v=0;v<n;v++){
+        scanf("%d", &adj[u][v]);
         }
     }
+    printf("Enter the source: ");
+    scanf("%d",&e);
+    BFS(e);
 
-    for(int i=0;i<e;i++){
-        int u,v;
-        printf("Enter the edges of (u,v): ");
-        scanf("%d %d",&u,&v);
-        adj[u][v]=1;
-        adj[v][u]=1;
-
+    printf("\nVertex\tdistance\tparent");
+    for(int i=0;i<n;i++){
+        printf("%d\t%d\t\t%d\n",i,distance[i],parent[i]);
     }
-    int s;
-    printf("Enter the source :\n ");
-    scanf("%d",&s);
-    BFS(s);
-
-    printf("\nVertex\tDistance\tPrevious\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d\t%d\t\t%d\n", i, distance[i], prev[i]);
-    }
-
-    return 0;
-
 
 }
+
